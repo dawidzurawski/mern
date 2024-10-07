0d011ba117c97dfd5c794609400272a05240ef78
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-10-07 02:29:03 +0100

    API WIP

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,43 @@
 import express from 'express'
 import dotenv from "dotenv"
+import { connectDB } from './config/db.js'
+import Product from './models/product.model.js'
 
 dotenv.config()
 
 const app = express()
 
-app.get("/products", (req, res) => {
-  res.send("Server is ready")
+app.use(express.json())
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if(!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all of the fields"
+    })
+  }
+
+  const newProduct = new Product(product)
+
+  try {
+    await newProduct.save()
+    res.status(200).json({
+      success: true,
+      data: newProduct
+    })
+  } catch (error) {
+     console.error("Error in creating product:", error.message)
+     res.status(500).json({
+      success: false,
+      message: "Server Error"
+     })
+  }
 })
 
-console.log(process.env.MONGO_URI)
-
 app.listen(5000, () => {
+  connectDB()
   console.log("Server started at http://localhost:5000")
 })
 
