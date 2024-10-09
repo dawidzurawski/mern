772240beb1e191f3bcc545e2175502e2c4106d16
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-10-09 10:28:43 +0100

    Fix error

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from "@chakra-ui/react"
+import { Box, Button, Container, Heading, Input, useColorModeValue, useToast, VStack } from "@chakra-ui/react"
 import { useState } from "react"
 import { useProductStore } from "../store/product"
 
@@ -9,11 +9,26 @@ const CreatePage = () => {
     image: ""
   })
 
+  const toast = useToast()
   const { createProduct } = useProductStore()
 
   const handleAddProduct = async () => {
     const { success, message } = await createProduct(newProduct)
-    console.log("Success:", success)
+    if(!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true
+      })
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        isClosable: true
+      })
+    }
     console.log("Message:", message)
   }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,20 @@
+import { Container } from "@chakra-ui/react"
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW='container.xl' py={12}>
+      <VStackContainer spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+      </VStackContainer>
+    </Container>
   )
 }
 
